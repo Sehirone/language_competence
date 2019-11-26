e5c74158b1265558cc5be07033aaa369686386a5
Kajetan <kajetan123@gmail.com>
2019-11-26 17:33:24 +0100

    cleanup test functions

--- language_evaluator/views.py
+++ language_evaluator/views.py
@@ -27,48 +27,6 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-# TODO: Remove
-def question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-    answer_set = question.answer_set.all()
-    template = loader.get_template('language_evaluator/question.html')
-    context = {
-        'question': question,
-        'answer_set': answer_set,
-    }
-
-    return HttpResponse(template.render(context, request))
-
-
-# TODO: Remove
-def answer(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-
-    try:
-        selected_choice = question.answer_set.get(pk=request.POST['a'])
-    except (KeyError, Answer.DoesNotExist):
-        # Redisplay the question voting form.
-        answer_set = question.answer_set.all()
-        return render(request, 'language_evaluator/question.html', {
-            'question': question,
-            'answer_set': answer_set,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        return HttpResponseRedirect(reverse('answer_result', args=(question_id, selected_choice.id)))
-
-
-# TODO: Remove
-def answer_result(request, question_id, answer_id):
-    question = get_object_or_404(Question, pk=question_id)
-    answer = get_object_or_404(Answer, pk=answer_id)
-    return render(request, 'language_evaluator/answer_result.html', {'question': question, 'answer': answer})
-
-
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
