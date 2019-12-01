014d1ada0783aa94e11b948e2ff3f4b987d3c6aa
Kajetan <kajetan123@gmail.com>
2019-12-01 15:00:03 +0100

    add django.extensions && save database dump

--- language_competence/settings.py
+++ language_competence/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'language_evaluator.apps.LanguageEvaluatorConfig'
+    'language_evaluator.apps.LanguageEvaluatorConfig',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
